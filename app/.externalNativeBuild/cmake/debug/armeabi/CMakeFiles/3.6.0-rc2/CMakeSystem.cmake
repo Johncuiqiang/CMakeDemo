4e7e366f137186ce42494fc2b1a61f5bc123099c
cuiqiang <cuiqiang@ling.ai>
2018-04-26 19:21:29 +0800

    init

--- app/.externalNativeBuild/cmake/debug/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/debug/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.10.0-40-generic")
-set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.10.0-40-generic")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.16299")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.16299")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("/home/cuiqiang/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("E:/SDK/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-15")
 set(CMAKE_SYSTEM_NAME "Android")
